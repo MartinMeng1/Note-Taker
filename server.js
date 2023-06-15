99ad39fc574aba9c5d8054725fe888eedbc628d2
MartinMeng1 <126767342+MartinMeng1@users.noreply.github.com>
2023-06-14 18:31:29 -0700

    Changed.server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const express = require('express');
 const path = require('path');
 const { v4: uuidv4 } = require('uuid')
 
-const PORT =  3001;
+const port = process.env.PORT || 3001;
 
 const app = express();
 
